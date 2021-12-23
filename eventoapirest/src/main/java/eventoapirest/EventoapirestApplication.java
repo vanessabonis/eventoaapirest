83a9a331d9d11c1bc85ea68a795e45694cbda934
vanessabonis <vanessa1302@gmail.com>
2021-12-23 16:46:37 -0300

    dependencias add

---
+++ eventoapirest/src/main/java/eventoapirest/EventoapirestApplication.java
@@ -0,0 +1,13 @@
+package eventoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoapirestApplication.class, args);
+	}
+
+}
---
+++ eventoapirest/src/test/java/eventoapirest/EventoapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package eventoapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
